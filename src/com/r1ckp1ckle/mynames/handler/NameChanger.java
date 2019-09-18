a8c6f8f01a6d6b08f530b1c306997459232236e7
vict2292 <vict2292@gmail.com>
2019-09-18 13:12:32 +0200

    Added Crafted Event on item updating

--- src/com/r1ckp1ckle/mynames/handler/NameChanger.java
+++ src/com/r1ckp1ckle/mynames/handler/NameChanger.java
@@ -6,6 +6,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class NameChanger implements Listener {
@@ -22,5 +24,20 @@ public class NameChanger implements Listener {
         ItemMeta meta = event.getItem().getItemStack().getItemMeta();
         meta.setDisplayName(MyNamesCore.getInstance().getConfigurationUtils().getData().getString(event.getItem().getItemStack().getType().toString() + "$" + event.getItem().getItemStack().getData().toString() + ".tag"));
         event.getItem().getItemStack().setItemMeta(meta);
+    }
+
+    @EventHandler
+    void onItemCrafted(CraftItemEvent event) {
+        ItemStack itemStack = event.getRecipe().getResult();
+        if (!MyNamesCore.getInstance().getTweakedMats().contains(itemStack.getType().toString() + "$" + itemStack.getData().toString()))
+            return;
+        if (CraftItemStack.asNMSCopy(itemStack).hasTag()) {
+            if (CraftItemStack.asNMSCopy(itemStack).getTag().hasKey("r1ckp1ckle.tagged")) return;
+        }
+        if (itemStack.getItemMeta().getDisplayName().equals(MyNamesCore.getInstance().getConfigurationUtils().getData().getString(itemStack.getType().toString() + "$" + itemStack.getData().toString())))
+            return;
+        ItemMeta meta = itemStack.getItemMeta();
+        meta.setDisplayName(MyNamesCore.getInstance().getConfigurationUtils().getData().getString(itemStack.getType().toString() + "$" + itemStack.getData().toString() + ".tag"));
+        itemStack.setItemMeta(meta);
     }
 }
